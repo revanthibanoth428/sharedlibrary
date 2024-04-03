aa61a736b85ca7237a72743b2a7353d8a3ca4efd
chinni <chinni@gmail.com>
2024-04-03 16:46:41 +0530

    file has been modified

--- vars/welcomemessage.groovy
+++ vars/welcomemessage.groovy
@@ -2,3 +2,7 @@ def call() {
      echo "welcome to jenkins shared library"
 	  echo "updated the shared library with new script"
 }
+
+def Displayanothermessage(msg) {
+  echo "message received is: ${msg}"
+  }
